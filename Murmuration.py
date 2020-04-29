c9ba364f92a8a11b958c7b066faac3f1acdabea3
Aravindh Vasu Lap <raina2654@gmail.com>
2020-04-29 11:28:35 +0530

    Added multiple leaders

--- Murmuration.py
+++ Murmuration.py
@@ -5,23 +5,23 @@ from manimlib.imports import *
 class Boids(Scene):
     CONFIG = {
         "stay_away": 0.25,
-        "local_rad": 1,
-        "local_v_weight": 1,  # towards local center
-        "common_v_weight": 2.,  # towards leader
-        "leader_v_weight": 0.1,
+        "local_rad": 3,
+        "local_v_weight": 4,  # towards local center
+        "common_v_weight": 3.,  # towards leader
+        "leader_v_weight": 2,
         "push_v_weight": 2,
         "d": 0
     }
 
     def construct(self):
         birds = VGroup()
-        total_birds = 40
+        total_birds = 120
         boundary = Rectangle(height=FRAME_HEIGHT, width=FRAME_WIDTH)
         self.add(boundary)
 
         boxes = VGroup()
-        for _, direction in zip(range(4), [UR, DR, UL, DL]):
-            box = Rectangle(height=3, width=3).move_to(2.5 * direction)
+        for _, direction in zip(range(6), [UR, DR, UL, DL, 1.5 * RIGHT, 1.5 * LEFT]):
+            box = Rectangle(height=3, width=4).move_to(2.5 * direction)
             boxes.add(box)
             # self.add(box)
         bird_positions = []
@@ -33,16 +33,23 @@ class Boids(Scene):
                 interpolate(x0, x1, random.random()),
                 interpolate(y0, y1, random.random()),
                 0
-            ]) + box.get_center() for _ in range(int(total_birds / 4))]
+            ]) + box.get_center() for _ in range(int(total_birds / 6))]
             for nbp in nth_bird_positions:
                 bird_positions.append(nbp)
 
+        last_bird_of_nth_group = []
+        d = 0
+        for i in range(len(boxes)):
+            d += total_birds / len(boxes)
+            last_bird_of_nth_group.append(d)
+        print(last_bird_of_nth_group)
         # bird_positions = [np.random.uniform(-FRAME_HEIGHT, 0., size=3) for _ in range(int(total_birds / 2))]
         # bird_positions2 = [np.random.uniform(0, FRAME_HEIGHT, size=3) for _ in range(int(total_birds / 2))]
         # for shit in bird_positions2:
         #     bird_positions.append(shit)
         self.previous_bird_positions = bird_positions
-        self.centroid = np.mean(bird_positions, axis=0)
+        # self.pseudo_lead_right = RIGHT * 3
+        # self.pseudo_lead_left = LEFT * 3
 
         def check_boundary(mobj):
             if mobj.get_left()[0] < boundary.get_left()[0]:
@@ -57,11 +64,9 @@ class Boids(Scene):
             if mobj.get_top()[1] > boundary.get_top()[1]:
                 mobj.velocity[1] = -abs(mobj.velocity[1])
 
-        def update_centroid(center, dt):
-            # center.velocity = UP * dt * 2
-            self.centroid += center.velocity
-            check_boundary(center)
-            center.move_to(self.centroid)
+        def update_pseudo_lead(leader, dt):
+            leader.shift(leader.velocity * dt)
+            check_boundary(leader)
 
         def get_dist(v1, v2):
             return np.linalg.norm(v1.get_center() - v2.get_center())
@@ -74,71 +79,75 @@ class Boids(Scene):
                         bird.velocity += - (b.get_center() - bird.get_center()) * ((-self.push_v_weight * 10 * dist / self.stay_away) + self.push_v_weight * 10)
 
         def update_bird(bird, dt):
-            s = self.common_v_weight
-            direc = (self.centroid - bird.get_center()) / np.linalg.norm(self.centroid - bird.get_center())
-            # if bird.get_center()[1] >= self.centroid[1]:
+
+            bird_index = 0
+            for index, b in enumerate(birds):
+                if b is bird:
+                    bird_index = index
+                    # print(bird_index)
+            exists = False
+            for i in range(len(boxes)):
+                if bird_index < last_bird_of_nth_group[i]:
+                    direc = (leaders[i].get_center() - bird.get_center()) / np.linalg.norm(leaders[i].get_center() - bird.get_center())
+                    exists = True
+                    break
+
+            if not exists:
+                direc = (leaders[0].get_center() - bird.get_center()) / np.linalg.norm(leaders[0].get_center() - bird.get_center())
+
+            # if bird.get_center()[1] >= self.pseudo_lead_right[1]:
             #     bird.rotate(-angle_between(bird.get_vertices()[0] - bird.get_center(), direc))
             # else:
             #     bird.rotate(angle_between(bird.get_vertices()[0] - bird.get_center(), direc))
-            bird.velocity = 1 * s * direc
+
+            bird.velocity = self.common_v_weight * direc
             lb_positions = []
-            # print(bird_positions)
             for pbp in self.previous_bird_positions:
-                # print(pbp)
-                # print(np.array_equal(pbp, bird.get_center()))
+
                 if not np.array_equal(pbp, bird.get_center()):
-                    # print(self.local_rad)
                     if np.linalg.norm(pbp - bird.get_center()) < self.local_rad:
-                        # print("Phew")
                         lb_positions.append(pbp)
 
-            # print("Starts")
-            # print(lb_positions)
-            # print("Ends")
             local_centroid = np.mean(lb_positions, axis=0)
-            # print(local_centroid)
-            # print()
 
             local_direc = (local_centroid - bird.get_center()) * self.local_v_weight
-            # print(local_direc)
             bird.velocity += local_direc
 
             dist_check(bird)
             check_boundary(bird)
 
             bird.shift(bird.velocity * dt)
-            # bird.shift(bird.dist_velocity * dt * 0.25)
+
             if bird is last_bird:
-                # print(birds[0].get_center())
                 self.previous_bird_positions = [b.get_center() for b in birds]
-                # print(self.previous_bird_positions)
-            # print("RENEWED")
 
         last_bird = Dot(np.random.uniform(-3., -1., size=3), radius=0.05, color=RED)
 
-        center = Dot(self.centroid)  # , radius=0.001)
-        center.velocity = UR * self.leader_v_weight
-        center.add_updater(update_centroid)
-        self.add(center)
+        leaders = VGroup()
+        for i in range(len(boxes)):
+            leader = Dot(boxes[i].get_center())  # , radius=0.001)
+            leader.velocity = rotate_vector(RIGHT, 2 * PI * random.random()) * self.leader_v_weight
+            leaders.add(leader)
+
+        for leader in leaders:
+            leader.add_updater(update_pseudo_lead)
+            self.add(leader)
 
         for i in range(total_birds):
             bird = Dot(bird_positions[i], radius=0.05, color=RED)
             # bird = Triangle(color=BLUE, fill_opacity=1, fill_color=BLUE).rotate(-PI / 2).scale(0.5).move_to(bird_positions[i])
-
             birds.add(bird)
 
         birds.add(last_bird)
         last_bird.add_updater(update_bird)
 
         for bird in birds:
-            s = 2
-            direc = (self.centroid - bird.get_center()) / np.linalg.norm(self.centroid - bird.get_center())
-            bird.velocity = 1 * s * direc
+            bird.velocity = RIGHT
             bird.add_updater(update_bird)
             # self.add(bird)
 
         self.add(birds)
         self.wait(5)
-        # print(self.centroid)
+        # print(self.pseudo_lead_right)
         # print(birds[0].get_center())
         # self.wait()
